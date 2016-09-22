ce9450f095f8acea6adac7f7f4a16b6c39ddd7d8
sunilmg1 <sunilvalliyil@gmail.com>
2016-09-22 13:50:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -46,38 +46,38 @@ var date = data.date;
 var heading = data.heading;
 var content = data.content;
 
-var htmltemplate = `
-
-<html>
-    <head>
-        <title>
-           ${title}
-        </title>
-        <meta name= "viewport" content= "width = device-width, inital-scale =1" />
-        <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-         <div class="container">
-            <div>
-                <a href="/">Home</a>
+var htmltemplate = 
+`
+    <html>
+        <head>
+            <title>
+               ${title}
+            </title>
+            <meta name= "viewport" content= "width = device-width, inital-scale =1" />
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+             <div class="container">
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${date}
+                </div>
+                <div>
+                    ${content}
+                
+                </div>
             </div>
-            <hr/>
-            <h3>
-                ${heading}
-            </h3>
-            <div>
-                ${date}
-            </div>
-            <div>
-                ${content}
-            
-            </div>
-        </div>
-    </body>
-</html>
+        </body>
+    </html>
 `;
 return htmltemplate;
-}
+};
 
 
 app.get('/', function (req, res) {
