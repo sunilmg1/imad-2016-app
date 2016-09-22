19d9e1cbfeb026af76d2630eaea1bddcf2e1cfd6
sunilmg1 <sunilvalliyil@gmail.com>
2016-09-22 15:40:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ return htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/:articleName', function (req, res) {
+app.get('/articleName', function (req, res) {
     var articleName = req.params.articleName;
     res.send(createTemplate(articles[atrticleName]));
 });
