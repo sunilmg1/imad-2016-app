426cadf4ee6ab85ab844b88fc44fcf98fbe77bba
sunilmg1 <sunilvalliyil@gmail.com>
2016-10-02 21:42:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -97,8 +97,8 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/sunil.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'sunil.png'));
 });
 
 
