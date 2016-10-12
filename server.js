764059b311de8e079a99c458c7e9252041843095
sunilmg1 <sunilvalliyil@gmail.com>
2016-10-12 16:47:33 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,6 +109,12 @@ app.get('/ui/sunil.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'sunil.png'));
 });
 
+var names = [];
+app.get('/submit-name/:name', function(req,res) {
+    var name=req.params.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
