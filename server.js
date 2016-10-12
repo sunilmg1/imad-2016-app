e07bf631fc339660658db7d7bfac6771b1a5e67a
sunilmg1 <sunilvalliyil@gmail.com>
2016-10-12 19:01:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,8 +110,8 @@ app.get('/ui/sunil.png', function (req, res) {
 });
 
 var names = [];
-app.get('/submit-name/:names', function(req, res) {
-    var name = req.params.names;
+app.get('/submit-name', function(req, res) {
+    var name = req.query.names;
     names.push(name);
     res.send(JSON.stringify(names));
 });
