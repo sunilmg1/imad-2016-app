2a04f3b470410db09be2db47adaea9b83790d471
sunilmg1 <sunilvalliyil@gmail.com>
2016-10-12 17:06:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -92,7 +92,7 @@ app.get('/counter', function (req,res){
 });
 
 var names = [];
-app.get('/submit-name', function(req,res) {
+app.get('/submit-name', function(req, res) {
     var name = req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
